acb95c4b2409b44c13e8238b8c53ab6cf28b1ded
Zacaria <zc@42.am>
2017-03-07 21:05:29 +0100

    [ADD] : exercise 2

--- src/components/questions/Question2.jsx
+++ src/components/questions/Question2.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Example from '../patterns/RenderCallback/example'
-//import Exercise from '../patterns/RenderCallback/exercise/GitHubIssueSearchInfiniteScroller'
-import Exercise from '../patterns/RenderCallback/exercise/'
+import Exercise from '../patterns/RenderCallback/exercise/GitHubIssueSearch'
+// import Exercise from '../patterns/RenderCallback/exercise/GitHubIssueSearchInfiniteScroller'
+// import Exercise from '../patterns/RenderCallback/exercise/'
+import ScrollNotifier from './../patterns/RenderCallback/exercise/utils/ScrollNotifier'
 
 const Question2 = (props) => (
   <div>
@@ -13,7 +15,32 @@ const Question2 = (props) => (
     </div>
     <h3>Exercise</h3>
     Refactor the HoCs GitHubIssueSearchInfiniteScroller to be a Render Callback
-    <Exercise />
+    <Exercise>
+      {
+          // Inject logic
+        (issues, fetchNextPage, isFetching) => (
+          <ScrollNotifier
+            style={{ height: 400 }}
+            onScrollAtTheBottom={ fetchNextPage }
+          >
+          <ul className="issue-list">
+            {issues.map(item => (
+              <li key={item.id}>
+                  <a href={item.html_url} target="_blank">
+                      <h3>{item.title}</h3>
+                      <p>{item.body}</p>
+                  </a>
+              </li>
+            ))}
+            {!isFetching ? null : <li className="text-center" key="loading">
+                  <h2><i className="fa fa-cog fa-spin fa-3x fa-fw margin-bottom"></i></h2>
+              </li>
+            }
+          </ul>
+          </ScrollNotifier>
+        )
+      }
+    </Exercise>
     <hr />
   </div>
 )
